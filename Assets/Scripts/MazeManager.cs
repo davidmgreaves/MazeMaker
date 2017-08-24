f8057746e9a7490aa7eca03ed930ae11031a3143
Dave Greaves <davidmgreaves@gmail.com>
2017-08-25 01:36:26 +1000

    Created MazeMaker prefab and added a MazeManager object to the main scene, with scripted functions to handle initial player input, as well as loading and restarting the maze

---
+++ Assets/Scripts/MazeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeManager : MonoBehaviour {
+
+	public MazeMaker mazeMakerPrefab;
+
+	private MazeMaker mazeMakerInstance;
+
+	void Start () {
+		LoadMaze();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Enter))
+		{
+			RestartMaze();
+		}
+	}
+	
+	void LoadMaze () {
+		mazeMakerInstance = Instantiate(mazeMakerPrefab) as MazeMaker;
+	}
+
+	void RestartMaze ()
+	{
+		Destoy(mazeMakerInstance.gameObject);
+		LoadMaze;
+	}
+}
